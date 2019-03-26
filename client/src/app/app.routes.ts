73eb1cfc6a35621a801ac13b1da5198d2d7ee00a
ColtDahl <dahlcolt@gmail.com>
2019-03-26 13:59:57 -0500

    Reconnect Home page in preperation for authentification system
    This will make it easier to work with the Google API.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RideListComponent} from "./rides/ride-list.component";
 import {AddRideComponent} from "./rides/add-ride.component";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
 
 // Route Configuration
 export const routes: Routes = [
-  {path: '', component: RideListComponent},
+  {path: '', component: HomeComponent},
   {path: 'rides', component: RideListComponent},
   {path: 'addride', component: AddRideComponent}
 
