77d4b8cded290bf2050d3d3017f6319018e1d412
UtkarshKr007 <kumar375@morris.umn.edu>
2019-04-10 10:04:46 -0500

    import AppService to app.component.ts and make navigation menu only visible if appService.isSignedIn is true

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
 export class AppComponent {
   title = 'Mongo-Angular-Spark lab';
+
+  constructor(public appService: AppService) {}
 }
