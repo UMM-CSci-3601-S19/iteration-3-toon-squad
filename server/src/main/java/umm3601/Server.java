5284d37cd8d97741cbb4a8592fa0a428cf9e24ec
michael-small <small203@morris.umn.edu>
2019-04-13 23:07:03 -0500

    Implement "api/rides/remove" route to server, enabling ride deletion
    Code from iteration-2-bananya-squad (Utkarsh's group)
    
    #32

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -84,6 +84,7 @@ public class Server {
     get("api/rides", rideRequestHandler::getRides);
     get("api/rides/:id", rideRequestHandler::getRideJSON);
     post("api/rides/new", rideRequestHandler::addNewRide);
+    post("api/rides/remove", rideRequestHandler::deleteRide);
 
     // An example of throwing an unhandled exception so you can see how the
     // Java Spark debugger displays errors like this.
