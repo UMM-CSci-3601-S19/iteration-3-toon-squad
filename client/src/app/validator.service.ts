3156868c6854f3477223abba16a68104fe83b641
LeahLuLu <juddx097@morris.umn.edu>
2019-04-15 18:53:23 -0500

    Create form validating service that all components can use
    No longer need to make form validation in every single component.

---
+++ client/src/app/validator.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
+
+@Injectable()
+
+export class ValidatorService {
+
+  public rideForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ride_validation_messages = {
+
+    'seatsAvailable': [
+      {type: 'required', message: 'Please specify how many seats you\'re offering'},
+      {type: 'min', message: 'Please offer at least 1 seat'},
+      {type: 'max', message: 'Can\'t offer more than 12 seats'},
+    ],
+
+    'origin': [
+      {type: 'required', message: 'Origin is required'}
+    ],
+
+    'destination': [
+      {type: 'required', message: 'Destination is required'}
+    ],
+
+    'driving' : [
+      {type: 'required', message: 'You must indicate whether you are the driver or not'},
+    ]
+  };
+
+  createForm() {
+
+    this.rideForm = this.fb.group({
+
+      origin: new FormControl('origin', Validators.compose([
+        Validators.required
+      ])),
+
+      destination: new FormControl('destination', Validators.compose([
+        Validators.required
+      ])),
+
+      seatsAvailable: new FormControl('seatsAvailable', Validators.compose([
+        Validators.required,
+        Validators.min(1),
+        Validators.max(12)
+      ])),
+
+      driving: new FormControl('driving', Validators.compose([
+        Validators.required
+      ])),
+
+      departureDate: new FormControl('departureDate'),
+
+      departureTime: new FormControl('departureTime'),
+
+      notes: new FormControl('notes'),
+
+      nonSmoking: null
+    });
+  }
+
+}
