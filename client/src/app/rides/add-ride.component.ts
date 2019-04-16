db8fb3f52e54d838a5941f7d80fd40296e083081
michael-small <small203@morris.umn.edu>
2019-04-15 21:40:50 -0500

    Remove imports from component's validation
    The validation service imports are the only needed imports now.

--- client/src/app/rides/add-ride.component.ts
+++ client/src/app/rides/add-ride.component.ts
@@ -1,6 +1,5 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ride} from './ride';
-import {FormControl, Validators, FormGroup, FormBuilder} from "@angular/forms";
 import {RideListService} from "./ride-list.service";
 import {Observable} from "rxjs/Observable";
 import {ValidatorService} from "../validator.service";
