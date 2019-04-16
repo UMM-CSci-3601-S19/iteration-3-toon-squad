720755bf03ea315292eafc94e78b9563b6b61523
ColtDahl <dahlcolt@gmail.com>
2019-04-16 13:15:25 -0500

    Move inline css into scss file, add scss file to StyleURLs

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,7 +2,9 @@ import {Component} from '@angular/core';
 import {AppService} from "../app.service";
 
 @Component({
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styleUrls: ['./home.component.scss'],
+
 })
 
 export class HomeComponent {
