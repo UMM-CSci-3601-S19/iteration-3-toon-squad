859f449a1ae6ca4381abff8f6a17be3c66fabefc
nkfoss <nathankfoss@gmail.com>
2019-04-21 19:46:04 -0500

    Add date/time parsers to profile component
    Date and time were showing up in ISO format, and unspecified times and
    dates were also showing up in wrong format.

--- client/src/app/users/profile.component.ts
+++ client/src/app/users/profile.component.ts
@@ -25,6 +25,62 @@ export class ProfileComponent implements OnInit{
     this.createForm();
   }
 
+  public checkImpossibleDate(ride: Ride) {
+    return (ride.departureDate.includes("3000"))
+  }
+
+  public checkImpossibleTime(ride: Ride) {
+    return (ride.departureTime.includes("99") || ride.departureTime === "")
+  }
+
+  /**
+   * Parses ISO dates for human readable month/day, adds ordinal suffixes
+   * @param {string} selectedDate The date to be parsed, an ISO string like "2019-04-10T05:00:00.000Z"
+   * @returns {string} Returns human readable date like "April 12th"
+   */
+  public dateParse(selectedDate: string) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August",
+      "September", "October", "November", "December"];
+    const dateDateFormat = new Date(selectedDate);
+    const dateFullMonth = months[dateDateFormat.getMonth()];
+    let date = dateDateFormat.getDate().toString();
+    if (date === '1' || date === '21' || date === '31') {
+      date += 'st';
+    } else if (date === '2' || date === '22') {
+      date += 'nd';
+    } else if (date === '3' || date === '23') {
+      date += 'rd';
+    } else {
+      date += 'th';
+    }
+
+    return dateFullMonth + " " + date;
+  }
+
+  /**
+   * Converts 24 hour time to AM/PM (modified from Tushar Gupta @ https://jsfiddle.net/cse_tushar/xEuUR/)
+   * @param {string} time The time to be parsed in 24 hour format, 00:00 to 23:59.
+   * @returns {string} formats time like "12:00 AM" or "11:59 PM"
+   */
+  public hourParse(time) {
+    let hours = time.substring(0,2);
+    let min = time.substring(3,5);
+    if(hours == 0) {
+      return '12:' + min + ' AM';
+    } else if (hours == 12) {
+      return '12:' + min + ' PM';
+    } else if (hours < 12) {
+      if(hours<10){return hours[1] + ':' + min + ' AM';} //strip off leading 0, ie "09:XX" --> "9:XX"
+      else{return hours + ':' + min + ' AM';}
+    } else {
+      hours = hours - 12;
+      hours = (hours.length < 10) ? '0' + hours:hours;
+      return hours + ':' + min + ' PM';
+    }
+  }
+
+
+
   public getLocalUserId() {
     return localStorage.getItem("userId");
   }
